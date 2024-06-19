eeadb777f5ea4aa8b6ed3f6d30cdce6318e4b55f
heathersdgku <heatherchoe@gmail.com>
2024-06-18 19:45:45 -0700

    create

---
+++ test.py
@@ -0,0 +1,39 @@
+#Javascript way
+# create variable
+# let, var or const
+# let name = "adrian";
+
+name = "heather"
+last_name = "Choe"
+age = 37
+found = False
+print(name)
+
+
+
+# list .... these are the arrays
+colors = ["black", "white", "blue", 1]
+#add
+colors.append("purple")
+# get any element from array
+print(colors[0])
+
+# travel the array using a for loop
+for color in colors:
+    print(color)
+
+
+
+print(colors)
+
+#dictionary
+me = {
+    "age":24,
+    "firstName": "Heather",
+    "lastName": "Choe",
+}
+print(me)
+#modify
+me["age"]=99
+#get the value
+print(me["firstName"])
